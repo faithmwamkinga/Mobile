ba510fb1a04f47bd7632658da469254a98f0782d
faithmwamkinga <faithmwamkinga@gmail.com>
2023-02-02 22:54:40 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,14 @@
+fun main() {
+    var name = "Faithmwamkinga"
+    println(name)
+    var age: Byte =18
+    println(18)
+    var phonenumber: Long= +255788266616
+    println(+255788266616)
+    var weight:Short= 60
+    println(60)
+    val citizenshipIsKenyan: Boolean= false
+    println(citizenshipIsKenyan)
+
+
+}
